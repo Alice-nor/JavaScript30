c38ff22d4c45575e946bb6d40359b03f9deaf4be
Alice <a104040088@mail.shu.edu.tw>
2021-08-19 16:11:17 +0800

    JS #12 DONE

---
+++ 12 - Key Sequence Detection/README.md
@@ -0,0 +1,18 @@
+
+# 12 - Key Sequence Detection
+( 2021/8/19 DONE ) 類似玩遊戲時的組合技，有了 ABC 的組合，就會出現搭配成功的結果。
+成果：[12 - Key Sequence Detection](https://alice-nor.github.io/JavaScript30/11%20-%20Custom%20Video%20Player/index.html) 
+
+
+## JavaScript 筆記 ##
+
+今天的內容比較多是之前已經學過的內容。感覺很適合做其他的延伸。
+只有下面這一行要思考久一點我才比較理解....。
+
+```
+    // 不超出密碼之長度，並堆疊換掉第一個元素
+    // 這邊是 splice(-8, 0)，也就是刪去陣列中第 0 個數值
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+```
+
+
---
+++ 12 - Key Sequence Detection/script.js
@@ -0,0 +1,18 @@
+const pressed = []; // 儲存已輸入之內容
+const secretCode = 'unicorn'; // 設定好之技能名稱
+
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+
+    // 不超出密碼之長度，並堆疊換掉第一個元素
+    // 這邊是 splice(-8, 0)，也就是刪去陣列中第 0 個數值
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    if (pressed.join('').includes(secretCode)) {
+        console.log('DING DING!');
+        cornify_add();
+    }
+    console.log(pressed);
+})
